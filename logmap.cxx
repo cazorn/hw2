2d0d7e59859481c41fca4018e8b325a47a7f968f
Cazorn <cazorn@linux-ghqt.site>
2015-11-09 10:24:01 +0100

    lösung hw2

--- logmap.cxx
+++ logmap.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,17 +8,25 @@ int main(){
 	double x;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+	ofstream out("data.dat"); //create a file with the name: data, which stores all the calculations
+	int plenght = Nend-Nskip;
+	double p[plenght];
 
-	for(double r=0; r <= 4; r += 0.001){
+	for(double r=3; r <= 4; r += 1){
 	   x=x0;
 	   for(int i=1; i <= Nskip; i++)
 		   x = r*x*(1-x);
 	   for(int i=Nskip+1; i <= Nend; i++){
 	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
+			   p[i-Nskip-1]=x;
+	    }
+   	   for(int j=0;j <=Nend-Nskip-1;j++){   
+	    out << p[j] << endl; //subsequent writing in file  
+	    }
 	}
 
+	cout << "calculations done and sotred in file: data.dat" << endl;
+	out.close();
 
 	return 0;
 }
